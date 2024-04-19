1339700e8b44510bec4807e75231b86c87cce56f
SergioTanakaAtos <105051554+SerginhoTanaka@users.noreply.github.com>
2024-04-19 13:57:47 -0300

    fix

--- main.py
+++ main.py
@@ -1,47 +1,69 @@
-#streamlit
+# streamlit
 import streamlit as st
-#pandas
+# Pandas
 import pandas as pd
-#matplotlib
+# Matplotlib
 import matplotlib.pyplot as plt
-#python
+# Custom modules
 from preprocessing import Preprocessing
 from description import Description
-from typing import List, Optional
+# Typing
+from typing import List
+from typing import Optional
+
+"""
+This module provides a Dashboard class for data preprocessing and visualization.
+"""
 
 class Dashboard():
+    """
+    A class for data preprocessing and visualization.
+    """
+
     def __init__(self):
         self.data = pd.read_csv('novo_arquivo.csv', low_memory=False)
         self.preprocessor = Preprocessing(self.data)
         self.description = Description(self.data)
 
-
-    def _run(self)-> None:
+    def run(self) -> None:
+        """
+        Run the dashboard.
+        """
         st.sidebar.title("Pré-processamento de dados")
+        selected_option = st.sidebar.radio(
+            "Selecione uma opção",
+            ["Pré-processamento", "Descrição", "Gráfico sem pré-processamento"]
+        )
 
-        if st.sidebar.button("Pré-processamento"):
-            if st.sidebar.button("Fechar"):
-                st.sidebar.empty()
-            else:
-                self.preprocessor._select_preprocessing_method()
+        if selected_option == "Pré-processamento":
+            self.preprocessor._select_preprocessing_method()
 
-        if st.sidebar.button("Descrição"):
+        elif selected_option == "Descrição":
             self.description.run()
-        
-        if st.sidebar.button("Gráfico sem pré-processamento"):
-            data_headers = self.data.columns.tolist()
-            print(data_headers)
-            columns =  st.sidebar.multiselect(
+
+        elif selected_option == "Gráfico sem pré-processamento":
+            self.__generate_graph()
+
+    def __generate_graph(self) -> None:
+        """
+        Generate a graph without preprocessing.
+        """
+        st.sidebar.write("Gráfico sem pré-processamento")
+        data_headers = self.data.columns.tolist()
+        columns = st.sidebar.multiselect(
             'Selecione as colunas:',
             data_headers
-        )#pedir ajuda aqui também pois ele não deixa selecionar as colunas
-            # print(columns)
-            st.write("Selected Columns:", columns)
-            if columns:
-                clened_data = self.preprocessor._clean_data(self.data, columns)
-                self.generate_graph(clened_data,columns)
-            
-    def generate_graph(self,data, columns: Optional[List[str]] = ['situacao']) -> None:
+        )
+        if columns:
+            cleaned_data = self.preprocessor._clean_data(self.data, columns)
+            self.__plot_graph(cleaned_data, columns)
+
+    def __plot_graph(self, data, columns: Optional[List[str]] = None) -> None:
+        """
+        Plot a graph.
+        """
+        if columns is None:
+            columns = ['situacao']
         for col in columns:
             fig, ax = plt.subplots(figsize=(8, 6))
             col_data = data[col]
@@ -57,8 +79,7 @@ class Dashboard():
                 ax.text(i, value + 0.1, str(value), ha='center')
 
             st.pyplot(fig)
-    
 
 if __name__ == '__main__':
     dashboard = Dashboard()
-    dashboard._run()
+    dashboard.run()
--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import MaxAbsScaler
+
 #streamlit
 import streamlit as st
 #pandas
 import pandas as pd
 
-import matplotlib.pyplot as plt
 
 class Preprocessing():
     def __init__(self, data: pd.DataFrame):
@@ -24,8 +24,7 @@ class Preprocessing():
         preprocessing_method = st.sidebar.selectbox(
             'Selecione um método de normalização:',
             ('MinMaxScaler', 'StandardScaler', 'RobustScaler', 'Normalizer', 'MaxAbsScaler')
-        ) #pedir ajuda, pois ele deixa um método defalt,  não consigo escolher o método
-        print("allalalalalalla",preprocessing_method)
+        ) 
         self.__apply_preprocessing(preprocessing_method)
 
     def __apply_preprocessing(self,preprocessing_method: str) -> None:
@@ -42,8 +41,7 @@ class Preprocessing():
         clenaned_data = self._clean_data(self.data, self.data.columns.tolist())
         categorical_columns = clenaned_data.select_dtypes(include=['object']).columns
         
-        numerical_df  =  clenaned_data.drop(columns=categorical_columns)
-        # print("asjnhajsahksdakshd",numerical_df)
+        numerical_df = clenaned_data.drop(columns=categorical_columns)
 
         if self.scaler and numerical_df is not None:
             normalized_data = self.scaler.fit_transform(numerical_df)
