32c4511e42cb2a1026c700150d87777919dea5e8
SergioTanakaAtos <125500468+SergioTanakaAtos@users.noreply.github.com>
2024-09-10 11:45:06 -0300

    Update models.py

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class TBPrimaryActions(Base):
     dataset_name = Column(String, nullable=True)  # Nome do dataset utilizado
     is_ai = Column(Boolean, default=False)  # Indica se a ação envolve IA
     timestamp = Column(DateTime, default=datetime.utcnow)  # Momento da ação
-    ai_actions = relationship("DBAiActions", back_populates="primary_action", cascade="all, delete-orphan")  # Relação 1:N com AiActions
+    ai_actions = relationship("TBAiActions", back_populates="primary_action", cascade="all, delete-orphan")  # Relação 1:N com AiActions
 
 class TBAiActions(Base):
     __tablename__ = 'tb_ai_actions'
@@ -26,8 +26,8 @@ class TBAiActions(Base):
     model = Column(String, nullable=False)  # Nome do modelo utilizado (e.g., 'Random Forest')
     target_column = Column(String, nullable=False)  # Nome da coluna alvo
     metrics = Column(JSON, nullable=True)  # Métricas do modelo em formato JSON
-    primary_action_id = Column(Integer, ForeignKey('db_primary_actions.id'), nullable=False)  # Chave estrangeira para PrimaryActions
-    primary_action = relationship("DBPrimaryActions", back_populates="ai_actions")  # Relação N:1 com PrimaryActions
+    primary_action_id = Column(Integer, ForeignKey('tb_primary_actions.id'), nullable=False)  # Chave estrangeira para PrimaryActions
+    primary_action = relationship("TBPrimaryActions", back_populates="ai_actions")  # Relação N:1 com PrimaryActions
 
 
 Base.metadata.create_all(engine)
